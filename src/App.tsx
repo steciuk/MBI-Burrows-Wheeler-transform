7d4d2e594283a5e5f454018365fdd5d2777bfc67
Eryk <emroczko@me.com>
2022-10-22 11:13:46 +0200

    Deleted default page and added algorithm description

--- src/App.tsx
+++ src/App.tsx
@@ -6,16 +6,18 @@ import logo from './logo.svg';
 
 function App() {
 	return (
-		<div className="App">
-			<header className="App-header">
-				<img src={logo} className="App-logo" alt="logo" />
-				<p>
-					Edit <code>src/App.tsx</code> and save to reload.
-				</p>
-				<a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-					Learn React
-				</a>
+		<div className="app">
+			<header className="app-header">
+				<h1>Burrow-Wheeler Transform</h1>
 			</header>
+			<div className="algorithm">
+				<p>
+					The Burrows–Wheeler transform (BWT, also called block-sorting compression) rearranges a character <br></br>
+					string into runs of similar characters. This is useful for compression, since it tends to be easy to<br></br>
+					compress a string that has runs of repeated characters by techniques such as move-to-front transform<br></br>
+					and run-length encoding.
+				</p>
+			</div>
 		</div>
 	);
 }

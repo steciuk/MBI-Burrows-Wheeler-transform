e925a0b2716f4fa7cfbecd702d0f2afa3ea41bf6
Eryk <emroczko@me.com>
2022-10-23 18:20:30 +0200

    Added simple rendering bwt

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,10 @@ import './App.css';
 
 import React from 'react';
 
-import logo from './logo.svg';
+import { getBWT, printRotations, sortRotations } from './model/BWT';
+
+const rotations = printRotations('banana$');
+const sortedRotations = sortRotations(rotations);
 
 function App() {
 	return (
@@ -12,14 +15,40 @@ function App() {
 			</header>
 			<div className="algorithm">
 				<p>
-					The Burrows–Wheeler transform (BWT, also called block-sorting compression) rearranges a character <br></br>
-					string into runs of similar characters. This is useful for compression, since it tends to be easy to<br></br>
-					compress a string that has runs of repeated characters by techniques such as move-to-front transform<br></br>
+					The Burrows–Wheeler transform (BWT, also called block-sorting compression) rearranges a character{' '}
+					<br></br>
+					string into runs of similar characters. This is useful for compression, since it tends to be easy to
+					<br></br>
+					compress a string that has runs of repeated characters by techniques such as move-to-front transform
+					<br></br>
 					and run-length encoding.
 				</p>
+			</div>
+			<div>
+				<h1>Text: banana$</h1>
+				<h1>Rotations:</h1>
+				<ArrayPrinter arrayToPrint={rotations}></ArrayPrinter>
+				<h1>Sorted:</h1>
+				<ArrayPrinter arrayToPrint={sortedRotations}></ArrayPrinter>
+				<h1> BWT result:</h1>
+				<BwtResult sortedRotations={sortedRotations}></BwtResult>
 			</div>
 		</div>
 	);
 }
+
+const ArrayPrinter: React.FC<{ arrayToPrint: string[] }> = ({ arrayToPrint }) => {
+	return (
+		<ol>
+			{arrayToPrint.map((rot) => (
+				<li key={rot}>{rot}</li>
+			))}
+		</ol>
+	);
+};
+
+const BwtResult: React.FC<{ sortedRotations: string[] }> = ({ sortedRotations }) => {
+	return <p>{getBWT(sortedRotations)}</p>;
+};
 
 export default App;
